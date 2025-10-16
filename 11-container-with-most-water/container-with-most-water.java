d407f4293950ca4e0a85897ae16e47fd1ae1650f
Sudarshan Agrawal <sudarshanagrawal65@gmail.com>
2025-10-16 17:35:27 +0530

    Time: 3 ms (95.99%) | Memory: 58.1 MB (44.58%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static int ans(int s,int n,int[]arr){
+        int max=0;
+        while(s<=n){
+            int temp=Math.min(arr[s],arr[n])*((n)-s);
+            if (max<temp){
+                max=temp;
+            }
+            if(arr[s]<arr[n]){
+                s++;
+            }
+            else{
+                n--;
+            }
+        }
+        return max;
+    }
+    public int maxArea(int[] arr) {
+       int n=arr.length;
+       int max=ans(0,n-1,arr);
+       return max; 
+    }
+}
