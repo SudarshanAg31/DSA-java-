65782d77f20d45361fba0b0419c347b488706eeb
Sudarshan Agrawal <sudarshanagrawal65@gmail.com>
2026-05-22 00:54:39 +0530

    Time: 183 ms (74.63%) | Memory: 42.8 MB (69.82%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,72 +1,66 @@
 class Solution {
-public boolean check(char[][] board, int row, int col,char ch) {
+    int check1 = 0;
+
+    public boolean check(char[][] board, int row, int col, char ch) {
         int i = row;//0
         int j = col;//2
-            //check row
-            i = row;
-            j = col;
-            for (i = 0; i < board.length; i++) {
-                if (board[i][j] == ch)
+        //check row
+        i = row;
+        j = col;
+        for (i = 0; i < board.length; i++) {
+            if (board[i][j] == ch)
+                return false;
+        }
+        //check col
+        i = row;
+        j = col;
+        for (j = 0; j < board.length; j++) {
+            if (board[i][j] == ch)
+                return false;
+        }
+        //check 3X3
+        i = row;
+        j = col;
+        i = i / 3;
+        j = j / 3;
+        i *= 3;
+        j *= 3;
+        int e = i + 3;
+        int d = j + 3;
+        for (int a = i; a < e; a++) {
+            for (int b = j; b < d; b++) {
+                if (board[a][b] == ch)
                     return false;
             }
-            //check col
-            i = row;
-            j = col;
-            for (j = 0; j < board.length; j++) {
-                if (board[i][j] == ch)
-                    return false;
-            }
-            //check 3X3
-            i = row;
-            j = col;
-            i = i / 3;
-            j = j / 3;
-            i *= 3;
-            j *= 3;
-            int e = i + 3;
-            int d = j + 3;
-            for (int a = i; a < e; a++) {
-                for (int b = j; b < d; b++) {
-                    if (board[a][b] == ch)
-                        return false;
-                }
-            }
+        }
         return true;
     }
-    public void fun(char[][] board,int row,int col,char[][]arr){
-        if(col==board.length){
-            row+=1;
-            col=0;
+
+    public void fun(char[][] board, int row, int col) {
+        if (col == board.length) {
+            row += 1;
+            col = 0;
         }
-        if(row==board.length){
-            for(int i=0;i<board.length;i++){
-                for(int j=0;j<board.length;j++){
-                    arr[i][j]=board[i][j];
-                }
-            }
-            return ;
+        if (row == board.length) {
+            check1 = 1;
+            return;
         }
-        if(board[row][col]!='.'){
-            fun(board,row,col+1,arr);
-        }
-        else{
-            for(char i='1';i<='9';i++){
-            if(check(board,row,col,i)){
-                    board[row][col]=i;
-                    fun(board,row,col+1,arr);
-                    board[row][col]='.';
+        if (board[row][col] != '.') {
+            fun(board, row, col + 1);
+        } else {
+            for (char i = '1'; i <= '9'; i++) {
+                if (check(board, row, col, i)) {
+                    board[row][col] = i;
+                    fun(board, row, col + 1);
+                    if (check1 != 1)
+                        board[row][col] = '.';
                 }
             }
         }
     }
+
     public void solveSudoku(char[][] board) {
-        int n=board.length;
-        char[][]arr=new char [n][n];
-        fun(board,0,0,arr);
-        for(int i=0;i<board.length;i++){
-             for(int j=0;j<board.length;j++){
-                board[i][j]=arr[i][j];
-            }
-        }
+        fun(board, 0, 0);
+        check1 = 0;
     }
 }
