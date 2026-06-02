d1bd356f626f4ca417edfc0101f66d10ca161a44
Sudarshan Agrawal <sudarshanagrawal65@gmail.com>
2026-06-03 00:10:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (39.76%) - LeetSync

---
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -0,0 +1,25 @@
+class Solution {
+    public TreeNode insert(TreeNode root, int val) {
+        if (root.val > val) {
+            if (root.left == null)
+                root.left = new TreeNode(val);
+            else
+                insert(root.left, val);
+        }
+        if (root.val < val) {
+            if (root.right == null)
+                root.right = new TreeNode(val);
+            else
+                insert(root.right, val);
+        }
+        return root;
+    }
+
+    public TreeNode bstFromPreorder(int[] preorder) {
+        TreeNode root = new TreeNode(preorder[0]);
+        for (int i = 1; i < preorder.length; i++) {
+            root = insert(root, preorder[i]);
+        }
+        return root;
+    }
+}
