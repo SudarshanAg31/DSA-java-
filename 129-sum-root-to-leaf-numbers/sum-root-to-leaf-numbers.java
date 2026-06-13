228bdc7c220b601e3bd11aea99b8c1e27e53368e
Sudarshan Agrawal <sudarshanagrawal65@gmail.com>
2026-06-13 21:26:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (80.30%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -1,32 +1,12 @@
 
 class Solution {
-    public void fun(TreeNode root,List<String> temp,List<List<String>>ans){
-        if(root==null){
-            return;
-        }
-        temp.add(root.val+"");
-        if(root.left==null&&root.right==null){
-            ans.add(new ArrayList<>(temp));
-            temp.remove(temp.size() - 1);
-            return;
-        }
-        fun(root.left,temp,ans);
-        fun(root.right,temp,ans);
-        temp.remove(temp.size()-1);
-
+    public int fun(TreeNode root,int sum){
+        if(root==null)return 0;
+        sum=(sum*10)+root.val;
+        if(root.left==null&&root.right==null)return sum;
+        return fun(root.left,sum)+fun(root.right,sum);
     }
     public int sumNumbers(TreeNode root) {
-        List<List<String>>ans=new ArrayList<>();
-        List<String> temp=new ArrayList<>();
-        fun(root,temp,ans);
-        int total=0;
-        for (List<String> list : ans) {
-            String s = "";
-            for (String x : list) {
-                s += x;
-            }
-            if(!s.equals(""))total += Integer.parseInt(s);
-        }
-        return total;
+        return fun(root,0);
     }
 }
