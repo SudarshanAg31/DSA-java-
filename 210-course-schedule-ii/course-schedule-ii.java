64dc98f712fd56d193f4ec71269888539a9295ce
Sudarshan Agrawal <sudarshanagrawal65@gmail.com>
2026-06-20 17:26:13 +0530

    Time: 6 ms (44.50%) | Memory: 47 MB (41.41%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] findOrder(int n, int[][] pre) {
+        List<List<Integer>> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            arr.add(new ArrayList<>());
+        int[] incoming = new int[n];
+        for (int i = 0; i < pre.length; i++) {
+            int a = pre[i][0], b = pre[i][1];
+            arr.get(b).add(a);
+            incoming[a]++;
+        }
+        List<Integer> store = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (incoming[i] == 0) {
+                q.add(i);
+            }
+        }
+        while (!q.isEmpty()) {
+            int x = q.remove();
+            store.add(x);
+            for (Integer ele : arr.get(x)) {
+                incoming[ele]--;
+                if (incoming[ele] == 0) {
+                    q.add(ele);
+                }
+            }
+        }
+        if (store.size() == n) {
+            int[] final_ans = new int[n];
+            for (int i = 0; i < n; i++) {
+                final_ans[i] = store.get(i);
+            }
+            return final_ans;
+        } else
+            return new int[0];
+    }
+}
