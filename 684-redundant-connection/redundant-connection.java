414199bbc6355d213f42c14743795123cdd90930
Sudarshan Agrawal <sudarshanagrawal65@gmail.com>
2026-06-27 21:09:53 +0530

    Time: 1 ms (91.76%) | Memory: 45 MB (54.41%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.java
@@ -0,0 +1,42 @@
+class Solution {
+    static int[]p;
+    static int[]size;
+    public int find(int a){
+        if(p[a]==a)return a;
+        int led=find(p[a]);
+        p[a]=led;
+        return led;
+    }
+    public void union(int i,int j,int ans[]){
+        int a=find(i);
+        int b=find(j);
+        if(a==b){
+            ans[0]=i;
+            ans[1]=j;
+        }
+        else{
+            if(size[a]>size[b]){
+            p[b]=a;
+            size[a]++;
+            }
+            else{
+            p[a]=b;
+            size[b]++;
+            }
+        }
+    }
+    public int[] findRedundantConnection(int[][] edges) {
+        int n=edges.length;
+        p=new int[n+1];
+        size=new int[n+1];
+        for(int i=1;i<n+1;i++){
+            p[i]=i;
+            size[i]=1;
+        }
+        int ans[]=new int[2];
+        for(int i=0;i<n;i++){
+            union(edges[i][0],edges[i][1],ans);
+        }
+        return ans;
+    }
+}
