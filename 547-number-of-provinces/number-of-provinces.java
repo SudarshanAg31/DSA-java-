0463ba3a71ae34763528b3c852d200c874f3fabe
Sudarshan Agrawal <sudarshanagrawal65@gmail.com>
2026-06-27 20:49:50 +0530

    Time: 2 ms (43.26%) | Memory: 47.3 MB (42.82%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,32 +1,44 @@
 class Solution {
-    public int bfs(int[][] arr, boolean[] check) {
-        int n = arr.length;
-        int z = 0;
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (!check[i + 1]) {
-                q.add(i + 1);
-                check[i + 1] = true;
-                while (!q.isEmpty()) {
-                    int x = q.remove();
-                    for (int j = 0; j < n; j++) {
-                        if (arr[x - 1][j] == 1 && !check[j + 1]) {
-                            q.add(j + 1);
-                            check[j + 1] = true;
-                        }
-                    }
-                }
-                z++;
+    static int[] p;
+    static int[] size;
+    public int find(int a){
+        if(p[a]==a)return a;
+        int leader=find(p[a]);
+        p[a]=leader;
+        return leader;
+    }
+    public void union(int i,int j){
+        int a=find(i);
+        int b=find(j);
+        if(a!=b){
+            if(size[a]>size[b]){
+                p[b]=a;
+                size[a]+=size[b];
+            }
+            else{
+                p[a]=b;
+                size[b]+=size[a];
             }
         }
-        return z;
     }
-
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        boolean[] check = new boolean[n + 1];
-        int ans = bfs(isConnected, check);
-        return ans;
-
+        int n =isConnected.length;
+        p=new int[n+1];
+        size=new int[n+1];
+        for(int i=1;i<n+1;i++){
+            p[i]=i;
+            size[i]=1;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i+1!=j+1&&isConnected[i][j]==1)
+                union(i+1,j+1);
+            }
+        }
+        int count=0;
+        for(int i=1;i<n+1;i++){
+            if(p[i]==i)count++;
+        }
+        return count;
     }
 }
