6400d65f5cdb2608036312981613a8f1c91ca4e9
Sudarshan Agrawal <sudarshanagrawal65@gmail.com>
2026-08-16 23:46:02 +0530

    Time: 4 ms (75.68%) | Memory: 114.5 MB (94.59%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean stoneGameIX(int[] arr) {
+        int[] count = new int[3];
+        for (int i : arr) {
+            count[i % 3]++;
+        }
+        int a = count[1];
+        int b = count[2];
+        int c = count[0];
+        if (c % 2 == 0)
+            return a > 0 && b > 0;
+        return Math.abs(a - b) > 2;
+    }
+}
