426176aa2b4f5dc70fc7403519c17f8caa031bfe
Sudarshan Agrawal <sudarshanagrawal65@gmail.com>
2026-09-13 00:03:03 +0530

    Time: 41 ms (46.96%) | Memory: 199.7 MB (50.87%) - LeetSync

---
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
@@ -0,0 +1,37 @@
+class Solution {
+    public long countPairs(int n, int[][] edges) {
+        boolean []vis=new boolean [n];
+        Queue<Integer>q=new ArrayDeque<>();
+        List<List<Integer>>adj=new ArrayList<>();
+        for(int i=0;i<n;i++)adj.add(new ArrayList<>());
+        for(int i=0;i<edges.length;i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        long pair=0;
+        int remaining=n;
+        for(int i=0;i<n;i++){
+            int count=0;
+            if(!vis[i]){
+                q.add(i);
+                vis[i]=true;
+                count=1;
+            }
+            while(!q.isEmpty()){
+                int temp=q.remove();
+                for(int j:adj.get(temp)){
+                    if(!vis[j]){
+                        count++;    
+                        vis[j]=true;
+                        q.add(j);
+                    }
+                }
+            }
+            remaining -= count;
+            pair += (long) count * remaining;
+        }
+        return pair;
+    }
+}
