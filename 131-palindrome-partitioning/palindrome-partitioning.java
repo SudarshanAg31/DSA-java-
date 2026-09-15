489c2c46469d0f58978867b66eac230ce8e59ff1
Sudarshan Agrawal <sudarshanagrawal65@gmail.com>
2026-09-15 23:41:33 +0530

    Time: 14 ms (7.57%) | Memory: 65.7 MB (10.23%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,25 +1,25 @@
 class Solution {
-    public boolean check(String s){
-        String rev = new StringBuilder(s).reverse().toString();
-        return s.equals(rev);
+    public boolean check(String str){
+        return str.equals(new StringBuilder(str).reverse().toString());
     }
-    public void fun(List<List<String>> ans , ArrayList<String> list , String s , int i){
+    public void fun(String s,List<List<String>>ans,int i,List<String>temp){
         if(i==s.length()){
-            ans.add(new ArrayList<>(list));
-            return;
+            ans.add(new ArrayList(temp));
+            return;    
         }
-        for(int j = i ; j<s.length() ; j++){
-            if(check(s.substring(i , j+1))==true){
-                list.add(s.substring(i , j+1));
-                fun(ans , list , s , j+1);
-                list.remove(list.size()-1);
+        for(int j=i;j<s.length();j++){
+            String str=s.substring(i,j+1);
+            if(check(str)){
+                temp.add(str);
+                fun(s,ans,j+1,temp);
+                temp.remove(temp.size()-1);
             }
         }
     }
     public List<List<String>> partition(String s) {
-        List<List<String>> ans = new ArrayList<>();
-        ArrayList<String> list = new ArrayList<>();
-        fun(ans , list , s , 0);
+        List<List<String>>ans=new ArrayList<>();
+        List<String>temp=new ArrayList<>();
+        fun(s,ans,0,temp);
         return ans;
     }
 }
