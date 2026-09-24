aace8271c0591c02948617dd50245153dc06f63e
Sudarshan Agrawal <sudarshanagrawal65@gmail.com>
2026-09-25 00:01:04 +0530

    Time: 8 ms (73.78%) | Memory: 65.2 MB (58.11%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,17 +1,20 @@
 class Solution {
-    public boolean check(String str){
-        return str.equals(new StringBuilder(str).reverse().toString());
+    public boolean check(String s){
+        for(int i=0;i<s.length()/2;i++){
+            if(s.charAt(i)!=s.charAt(s.length()-1-i))return false;
+        }
+        return true;
     }
-    public void fun(String s,List<List<String>>ans,int i,List<String>temp){
-        if(i==s.length()){
-            ans.add(new ArrayList(temp));
-            return;    
+    public void fun(int i,String s,List<List<String>>ans, List<String>temp){
+        if(s.length()==i){
+            ans.add(new ArrayList<>(temp));
+            return ;
         }
         for(int j=i;j<s.length();j++){
-            String str=s.substring(i,j+1);
-            if(check(str)){
-                temp.add(str);
-                fun(s,ans,j+1,temp);
+            String sub=s.substring(i,j+1);
+            if(check(sub)){
+                temp.add(sub);
+                fun(j+1,s,ans,temp);
                 temp.remove(temp.size()-1);
             }
         }
@@ -19,7 +22,7 @@ class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>>ans=new ArrayList<>();
         List<String>temp=new ArrayList<>();
-        fun(s,ans,0,temp);
+        fun(0,s,ans,temp);
         return ans;
     }
 }
